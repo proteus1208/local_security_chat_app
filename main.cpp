75db6239de580def11fa1adddb33946eb287bc6a
proteus1208 <proteus1208@outlook.com>
2026-05-18 02:00:06 +0000

    [ui] set window icon

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QQuickStyle::setStyle("Fusion");
     MainWindow w;
+    w.setWindowIcon(QIcon("qml/assets/logo/logo.ico"));
+    w.setWindowTitle("SChat");
     w.show();
     return a.exec();
 }
